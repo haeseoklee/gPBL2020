a35505585e194438a5042bf91cc054bec97f1def
vagrant <vagrant@localhost.localdomain>
2020-02-18 01:22:52 +0000

    first commit

---
+++ resources/views/employee/index.blade.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<h1>Employees</h1>
+<ol>
+    @foreach ($employees as $emp)
+        <li>
+         employee number: {{ $emp->employee_number }} <br> 
+         name: {{$emp->name}} <br>
+         birthday: {{ $emp->birthday }} <br>
+         hometown: {{ $emp->hometown }} <br>
+         address: {{ $emp->address }} <br>
+         join date: {{ $emp->join_date }}
+         </li>
+    @endforeach
+</ol>
+</body>
+</html>
